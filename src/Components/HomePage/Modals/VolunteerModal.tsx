24e9e41c59a479aee7e9f337b8377fbe9ba30764
EngHikmat <hekmatmalkawi@hotmail.com>
2023-11-21 17:37:29 +0300

    Modal Implementation

--- src/Components/HomePage/Modals/VolunteerModal.tsx
+++ src/Components/HomePage/Modals/VolunteerModal.tsx
@@ -1,6 +1,8 @@
 import {
+  Button,
   FormControl,
   FormLabel,
+  HStack,
   Input,
   Modal,
   ModalCloseButton,
@@ -11,6 +13,7 @@ import {
   //   useToast,
 } from "@chakra-ui/react";
 import { MdGroups } from "react-icons/md";
+import { BsArrowLeft } from "react-icons/bs";
 
 interface Props {
   isOpen: boolean;
@@ -31,27 +34,116 @@ function VolunteerModal({ isOpen, onClose }: Props) {
       >
         <ModalCloseButton position="absolute" left="8px" top="8px" />
 
-        <VStack px="40px" py="40px" align="center" dir="rtl">
-          <MdGroups size="40px" />
-          <Text fontFamily="Readex Pro" fontSize="20px" fontWeight="600">
-            أرغب بالتطوع
-          </Text>
-          <Text
-            fontFamily="Readex Pro"
-            fontSize="16px"
-            fontWeight="400"
-            textAlign="center"
-            color="#374151"
-          >
-            هذه المبادرة لكل من يجد لديه القدرة على المساهمة في تطويرها بشكل
-            تطوعي كل حسب تخصصة
-          </Text>
+        <VStack px="40px" py="40px" align="stretch" dir="rtl">
+          <VStack>
+            <MdGroups size="40px" />
+            <Text fontFamily="Readex Pro" fontSize="20px" fontWeight="600">
+              أرغب بالتطوع
+            </Text>
+            <Text
+              fontFamily="Readex Pro"
+              fontSize="16px"
+              fontWeight="400"
+              textAlign="center"
+              color="#374151"
+            >
+              هذه المبادرة لكل من يجد لديه القدرة على المساهمة في تطويرها بشكل
+              تطوعي كل حسب تخصصة
+            </Text>
+          </VStack>
+
           <form>
-            <VStack align="stretch" w="100%">
-              <FormControl>
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
+            <VStack align="stretch" spacing="24px">
+              <VStack align="stretch" w="100%">
+                <FormControl w="100%">
+                  <FormLabel
+                    fontFamily="Readex Pro"
+                    fontSize="14px"
+                    fontWeight="400"
+                    mb="12px"
+                    color="#374151"
+                  >
+                    الإسم
+                  </FormLabel>
+                  <Input type="email" w="100%" rounded="full" h="48px" />
+                </FormControl>
+              </VStack>
+              <VStack align="stretch" w="100%">
+                <FormControl w="100%">
+                  <FormLabel
+                    fontFamily="Readex Pro"
+                    fontSize="14px"
+                    fontWeight="400"
+                    mb="12px"
+                    color="#374151"
+                  >
+                    البريد الإلكتروني
+                  </FormLabel>
+                  <Input type="email" w="100%" rounded="full" h="48px" />
+                </FormControl>
+              </VStack>
+              <VStack align="stretch" w="100%">
+                <FormControl w="100%">
+                  <FormLabel
+                    fontFamily="Readex Pro"
+                    fontSize="14px"
+                    fontWeight="400"
+                    mb="12px"
+                    color="#374151"
+                  >
+                    طبيعة العمل
+                  </FormLabel>
+                  <Input type="email" w="100%" rounded="full" h="48px" />
+                </FormControl>
+              </VStack>
+              <VStack align="stretch" w="100%">
+                <FormControl w="100%">
+                  <FormLabel
+                    fontFamily="Readex Pro"
+                    fontSize="14px"
+                    fontWeight="400"
+                    mb="12px"
+                    color="#374151"
+                  >
+                    عدد سنوات الخبرة
+                  </FormLabel>
+                  <Input type="email" w="100%" rounded="full" h="48px" />
+                </FormControl>
+              </VStack>
+              <VStack align="stretch" w="100%">
+                <FormControl w="100%">
+                  <FormLabel
+                    fontFamily="Readex Pro"
+                    fontSize="14px"
+                    fontWeight="400"
+                    mb="12px"
+                    color="#374151"
+                  >
+                    عدد الساعات التطوعية للمشروع
+                  </FormLabel>
+                  <Input type="email" w="100%" rounded="full" h="48px" />
+                </FormControl>
+              </VStack>
+              <Button
+                rounded="full"
+                h="48px"
+                bgGradient="linear(to-r, #282828, #205F45)"
+                _hover={{
+                  bgGradient: "linear(to-r, #282828, #205F45)",
+                }}
+                color="white"
+              >
+                <HStack justifyContent="space-between" w="100%">
+                  <Text
+                    fontFamily="Readex Pro"
+                    fontWeight="500"
+                    fontSize="16px"
+                  >
+                    إرسال
+                  </Text>
+                  <BsArrowLeft />
+                </HStack>
+              </Button>
             </VStack>
           </form>
         </VStack>
